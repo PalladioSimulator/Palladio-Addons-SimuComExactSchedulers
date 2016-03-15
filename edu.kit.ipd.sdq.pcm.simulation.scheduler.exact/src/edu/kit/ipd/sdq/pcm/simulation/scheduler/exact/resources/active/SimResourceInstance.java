af3afe933ea8a74ac5a74574e0a5e7bffaae3b8c
groenda <groenda@fzi.de>
2016-03-15 12:37:16 +0000

    Removed unnecessary dependency.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/SimuComExactSchedulers/trunk@31384 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/resources/active/SimResourceInstance.java
+++ edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/resources/active/SimResourceInstance.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
 
 import de.uka.ipd.sdq.scheduler.IActiveResource;
 import de.uka.ipd.sdq.scheduler.IRunningProcess;
