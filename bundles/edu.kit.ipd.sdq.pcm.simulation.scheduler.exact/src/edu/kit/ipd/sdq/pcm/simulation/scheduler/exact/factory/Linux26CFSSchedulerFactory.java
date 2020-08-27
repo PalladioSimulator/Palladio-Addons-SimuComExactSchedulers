fa92c510055af4aa80764fc32d755481193b460e
Martina Rapp <rapp@fzi.de>
2020-08-27 16:46:08 +0200

    refactoring; delegated IResourceTableManager as parameter to satisfy constructor

--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26CFSSchedulerFactory.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26CFSSchedulerFactory.java
@@ -3,14 +3,16 @@ package edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.factory;
 import de.uka.ipd.sdq.scheduler.IActiveResource;
 import de.uka.ipd.sdq.scheduler.SchedulerModel;
 import de.uka.ipd.sdq.scheduler.factory.SchedulerExtensionFactory;
+import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 import de.uka.ipd.sdq.scheduler.resources.active.SimProcessorSharingResource;
 
 
 public class Linux26CFSSchedulerFactory implements SchedulerExtensionFactory {
 
     @Override
-    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores) {
-        return new SimProcessorSharingResource(model, resourceName, resourceId, numberOfCores);
+    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores
+            , IResourceTableManager resourceTableManager) {
+        return new SimProcessorSharingResource(model, resourceName, resourceId, numberOfCores, resourceTableManager);
         //return new Linux26CFSResource(model, resourceName, resourceId);
     }
 
--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26O1SchedulerFactory.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26O1SchedulerFactory.java
@@ -3,15 +3,17 @@ package edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.factory;
 import de.uka.ipd.sdq.scheduler.IActiveResource;
 import de.uka.ipd.sdq.scheduler.SchedulerModel;
 import de.uka.ipd.sdq.scheduler.factory.SchedulerExtensionFactory;
+import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 import edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.ExactSchedulingFactory;
 
 
 public class Linux26O1SchedulerFactory implements SchedulerExtensionFactory {
 
     @Override
-    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores) {
+    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores
+            , IResourceTableManager resourceTableManager) {
         final ExactSchedulingFactory factory = new ExactSchedulingFactory(model);
-        return factory.getResource(model, "Linux 2.6.22", numberOfCores, "Utilisation of " + resourceName + " " + resourceId);
+        return factory.getResource(model, "Linux 2.6.22", numberOfCores, "Utilisation of " + resourceName + " " + resourceId, resourceTableManager);
     }
 
 }
