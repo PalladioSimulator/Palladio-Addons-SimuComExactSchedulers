f048c78d8340a5ad34bd237272a32e672f1fb0e7
Martina Rapp <rapp@fzi.de>
2020-10-06 09:00:10 +0200

    worked in review findings; reformatted source code file using Palladio code formatter

--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26O1SchedulerFactory.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26O1SchedulerFactory.java
@@ -6,14 +6,14 @@ import de.uka.ipd.sdq.scheduler.factory.SchedulerExtensionFactory;
 import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 import edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.ExactSchedulingFactory;
 
-
 public class Linux26O1SchedulerFactory implements SchedulerExtensionFactory {
 
     @Override
-    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores
-            , IResourceTableManager resourceTableManager) {
+    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName,
+            final String resourceId, final long numberOfCores, IResourceTableManager resourceTableManager) {
         final ExactSchedulingFactory factory = new ExactSchedulingFactory(model);
-        return factory.getResource(model, "Linux 2.6.22", numberOfCores, "Utilisation of " + resourceName + " " + resourceId, resourceTableManager);
+        return factory.getResource(model, "Linux 2.6.22", numberOfCores,
+                "Utilisation of " + resourceName + " " + resourceId, resourceTableManager);
     }
 
 }
