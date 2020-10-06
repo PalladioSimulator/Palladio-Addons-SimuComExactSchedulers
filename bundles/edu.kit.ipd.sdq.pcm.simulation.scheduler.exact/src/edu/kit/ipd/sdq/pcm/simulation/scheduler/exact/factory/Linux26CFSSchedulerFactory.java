4260cf761ff094c5d75f22db738b756ee04b1629
Martina Rapp <rapp@fzi.de>
2020-10-06 08:59:36 +0200

    worked in review findings; reformatted source code file using Palladio code formatter

--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26CFSSchedulerFactory.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/Linux26CFSSchedulerFactory.java
@@ -6,14 +6,13 @@ import de.uka.ipd.sdq.scheduler.factory.SchedulerExtensionFactory;
 import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 import de.uka.ipd.sdq.scheduler.resources.active.SimProcessorSharingResource;
 
-
 public class Linux26CFSSchedulerFactory implements SchedulerExtensionFactory {
 
     @Override
-    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores
-            , IResourceTableManager resourceTableManager) {
+    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName,
+            final String resourceId, final long numberOfCores, IResourceTableManager resourceTableManager) {
         return new SimProcessorSharingResource(model, resourceName, resourceId, numberOfCores, resourceTableManager);
-        //return new Linux26CFSResource(model, resourceName, resourceId);
+        // return new Linux26CFSResource(model, resourceName, resourceId);
     }
 
 }
