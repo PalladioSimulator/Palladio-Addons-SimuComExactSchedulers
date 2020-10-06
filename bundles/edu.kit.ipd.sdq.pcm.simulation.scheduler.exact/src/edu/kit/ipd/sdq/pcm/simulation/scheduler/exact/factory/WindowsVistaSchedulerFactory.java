7e101f0cf6d219acc60536ce7e60db5de5d109eb
Martina Rapp <rapp@fzi.de>
2020-10-06 09:01:35 +0200

    worked in review findings; reformatted source code file using Palladio code formatter

--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/WindowsVistaSchedulerFactory.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/factory/WindowsVistaSchedulerFactory.java
@@ -6,14 +6,14 @@ import de.uka.ipd.sdq.scheduler.factory.SchedulerExtensionFactory;
 import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
 import edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.ExactSchedulingFactory;
 
-
 public class WindowsVistaSchedulerFactory implements SchedulerExtensionFactory {
 
     @Override
-    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName, final String resourceId, final long numberOfCores
-            , IResourceTableManager resourceTableManager) {
+    public IActiveResource getExtensionScheduler(final SchedulerModel model, final String resourceName,
+            final String resourceId, final long numberOfCores, IResourceTableManager resourceTableManager) {
         final ExactSchedulingFactory factory = new ExactSchedulingFactory(model);
-        return factory.getResource(model, "Windows 2003", numberOfCores, "Utilisation of " + resourceName + " " + resourceId, resourceTableManager);
+        return factory.getResource(model, "Windows 2003", numberOfCores,
+                "Utilisation of " + resourceName + " " + resourceId, resourceTableManager);
     }
 
 }
