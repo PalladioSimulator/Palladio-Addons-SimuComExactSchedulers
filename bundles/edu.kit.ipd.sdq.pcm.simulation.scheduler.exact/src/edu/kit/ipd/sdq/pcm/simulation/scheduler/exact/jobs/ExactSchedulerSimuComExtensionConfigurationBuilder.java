c665a9fe6038aecb2e992eb1c444f4402738d050
Ralf Sieger <sieger@fzi.de>
2025-02-19 16:34:58 +0100

    adapted to moved SimuComConfig

--- bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/jobs/ExactSchedulerSimuComExtensionConfigurationBuilder.java
+++ bundles/edu.kit.ipd.sdq.pcm.simulation.scheduler.exact/src/edu/kit/ipd/sdq/pcm/simulation/scheduler/exact/jobs/ExactSchedulerSimuComExtensionConfigurationBuilder.java
@@ -3,7 +3,7 @@ package edu.kit.ipd.sdq.pcm.simulation.scheduler.exact.jobs;
 import java.util.Map;
 
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuComExtensionConfigurationBuilder;
-import de.uka.ipd.sdq.simucomframework.SimuComConfigExtension;
+import de.uka.ipd.sdq.simucomframework.core.SimuComConfigExtension;
 import de.uka.ipd.sdq.workflow.extension.AbstractExtensionJobConfiguration;
 
 public class ExactSchedulerSimuComExtensionConfigurationBuilder  extends SimuComExtensionConfigurationBuilder {
